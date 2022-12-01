5ebf533fe140c26ed534111e4ac261f84efebcc0
Martin Jankuliak <43377338+MartinJank@users.noreply.github.com>
2022-12-01 15:37:15 +0100

    random grid generator

---
+++ OG Tower defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour { 
+    private float enemyHealth; 
+    private float movementSpeed; 
+    private int killReward; // The amount of money the player gets when this enemy is killed 
+    private int damage; // The amount of damage the enemy does when it reaches the end 
+    private GameObject targetTile; 
+    private void Start() 
+    {
+
+    }
+
+    private void initializeEnemy() 
+    { 
+
+    }
+
+    private void moveEnemy(Vector2 targetPosition) 
+    { 
+        
+    } 
+
+    private void checkPosition() 
+    {
+
+    }
+    
+    private void Update() 
+    {
+
+    }
+}
