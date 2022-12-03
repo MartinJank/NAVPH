d0f0b966819c17b0f4511f1e5def82482124fcf8
David Korman <david.korman1@gmail.com>
2022-12-03 14:39:12 +0100

    enemy and towers basics

--- OG Tower defense/Assets/Scripts/Enemy.cs
+++ OG Tower defense/Assets/Scripts/Enemy.cs
@@ -3,33 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour { 
-    private float enemyHealth; 
-    private float movementSpeed; 
+    public static List<GameObject> enemies;
+    [SerializeField] private float enemyHealth; 
+    [SerializeField] private float movementSpeed; 
     private int killReward; // The amount of money the player gets when this enemy is killed 
     private int damage; // The amount of damage the enemy does when it reaches the end 
     private GameObject targetTile; 
+
+    private void Awake() {
+        Enemies.enemies.Add(gameObject);
+    }
+
     private void Start() 
     {
-
+        initializeEnemy();
     }
 
     private void initializeEnemy() 
     { 
-
+        targetTile = MapGenerator.startTile;
     }
 
-    private void moveEnemy(Vector2 targetPosition) 
+    public void takeDemage(float amount) {
+        enemyHealth -= amount;
+        if (enemyHealth <= 0) {
+            die();
+        }
+    }
+
+    private void die() {
+        Enemies.enemies.Remove(gameObject);
+        Destroy(transform.gameObject);
+    }
+
+    private void moveEnemy() 
     { 
-        
+        transform.position = Vector3.MoveTowards(transform.position, targetTile.transform.position, movementSpeed*Time.deltaTime);
     } 
 
     private void checkPosition() 
     {
-
+        if (targetTile != null && targetTile != MapGenerator.endTile) {
+            float distance  = (transform.position - targetTile.transform.position).magnitude;
+            if (distance < 0.001f) {
+                int currentIndex = MapGenerator.pathTiles.IndexOf(targetTile);
+                targetTile = MapGenerator.pathTiles[currentIndex + 1];
+            }
+        }
     }
     
     private void Update() 
     {
+        checkPosition();
+        moveEnemy();
 
+        takeDemage(0);
     }
 }
---
+++ OG Tower defense/Assets/Scripts/Tower.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+    [SerializeField] private float range;
+    [SerializeField] private float damage;
+    [SerializeField] private float attackSpeed;
+
+    private GameObject currentTarget;
+
+    private float nextTimeShoot;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextTimeShoot = Time.time;
+    }
+
+    private void updateNearestEnemy() {
+        GameObject currentNearestEnemy = null;
+        float distance = Mathf.Infinity;
+        foreach(GameObject enemy in Enemies.enemies) {
+            if (enemy != null) {
+                float _distance = (transform.position - enemy.transform.position).magnitude;
+                if (_distance < distance) {
+                    distance = _distance;
+                    currentNearestEnemy = enemy;
+                }
+            }
+        }
+
+        if (distance <= range) {
+            currentTarget = currentNearestEnemy;
+        } else {
+            currentTarget = null;
+        }
+    }
+
+    private void shoot() {
+        Enemy enemyScript = currentTarget.GetComponent<Enemy>();
+        enemyScript.takeDemage(damage);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        updateNearestEnemy();
+
+        if (Time.time >= nextTimeShoot) {
+            if (currentTarget != null) {
+                shoot();
+                nextTimeShoot = Time.time + attackSpeed;
+            }
+        }
+        shoot();
+    }
+}
